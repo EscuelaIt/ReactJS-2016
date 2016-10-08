58a096f7bc44b5981e5ba5a1051e9c93dc051c09
danderu <mail@dandel.es>
2016-10-08 14:35:13 +0200

    added header

---
+++ Sesiones/Sesion-08/src/containers/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Header } from '../components'
+
+const App = () => (
+  <Header />
+)
+
+export default App
--- Sesiones/Sesion-08/src/index.js
+++ Sesiones/Sesion-08/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import App from './containers/App'
 
-const SampleSpa = ({props}) => (
-  <div>Your module is working!</div>
+const SampleSpa = () => (
+  <App />
 )
 
 export default SampleSpa
