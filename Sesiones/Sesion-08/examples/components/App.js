b765f32fff8235e6e15e263f3551d39d6a3c5593
danderu <mail@dandel.es>
2016-10-08 14:03:33 +0200

    :fire: first commit

---
+++ Sesiones/Sesion-08/examples/components/App.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import SampleSpa from '@typeform/sample-spa'
+
+export default () => <SampleSpa />
---
+++ Sesiones/Sesion-08/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SampleSpa = ({props}) => (
+  <div>Your module is working!</div>
+)
+
+export default SampleSpa
