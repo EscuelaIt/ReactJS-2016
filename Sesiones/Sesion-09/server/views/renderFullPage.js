71932c4593dd7e98b5b91f5f96995fd8572ab588
danderu <mail@dandel.es>
2016-10-09 14:15:19 +0200

    sort scripts and bundles

--- Sesiones/Sesion-09/server/views/renderFullPage.js
+++ Sesiones/Sesion-09/server/views/renderFullPage.js
@@ -4,23 +4,18 @@ function renderFullPage (html, preloadedState) {
     <html>
       <head>
         <title>Redux Universal Example</title>
-        <link rel="stylesheet" href="/dist/sample-spa.min.css">
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width">
-        <!-- Compiled and minified CSS -->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
-        <!-- jQuery -->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <!-- Compiled and minified JavaScript -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
-        <!-- Font icons -->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="/dist/sample-spa.min.css">
       </head>
       <body>
         <div id="root">${html}</div>
         <script>
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
         </script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
         <script src="/dist/sample-spa.min.js"></script>
       </body>
     </html>
