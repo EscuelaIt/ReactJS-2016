4d6dd4cb221bb49cd4090444cba10eef73a6e18e
danderu <mail@dandel.es>
2016-10-13 19:29:31 +0200

    moved content

---
+++ Sesiones/Sesion-09/generator-example/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const GeneratorExample = ({props}) => (
+  <div>Your module is working!</div>
+)
+
+export default GeneratorExample
---
+++ Sesiones/Sesion-09/generator-example/webpack.config.gh-pages.babel.js
@@ -0,0 +1,50 @@
+import {optimize, DefinePlugin} from 'webpack'
+import {resolve, join} from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+const SRC_PATH = join(__dirname, '/src')
+const EXAMPLES_PATH = resolve(__dirname, 'examples')
+
+export default {
+  entry: EXAMPLES_PATH,
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+      exclude: /node_modules/
+    }]
+  },
+  exterals: {
+    'react': 'react',
+    'react-dom': 'react-dom'
+  },
+  output: {
+    path: join(EXAMPLES_PATH, 'dist'),
+    filename: 'gh-pages.min.js',
+    library: 'gh-pages',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new ExtractTextPlugin('gh-pages.min.css'),
+    new optimize.OccurenceOrderPlugin(),
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      'generator-example': SRC_PATH
+    },
+    extensions: ['', '.js', '.scss']
+  }
+}
