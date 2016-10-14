88d7e7b85b6abca551c1914e1152ca4bef3c3c58
danderu <mail@dandel.es>
2016-10-14 16:12:19 +0200

    add generator

---
+++ Sesiones/Sesion-09/generator-escuela-it-react/README.md
@@ -0,0 +1,8 @@
+# generator-escuela-it
+A generator for starting with new front end projects
+
+## Installation
+TODO
+
+## Development
+TODO
---
+++ Sesiones/Sesion-09/generator-escuela-it-react/generators/app/templates/src/_index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const <%= pascalName %> = ({props}) => (
+  <div>Your module is working!</div>
+)
+
+export default <%= pascalName %>
