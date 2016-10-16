166d1f7eff8699dc10a604a9c3dbbcba4c870e43
danderu <mail@dandel.es>
2016-10-16 13:06:59 +0200

    improve code coverage

--- Sesiones/Sesion-10/test/components/searchFilters.test.js
+++ Sesiones/Sesion-10/test/components/searchFilters.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import { expect } from 'chai'
 import sinon from 'sinon'
 
@@ -12,5 +12,26 @@ describe('<SearchFilters />', () => {
 
     wrapper.find('input').simulate('change')
     expect(onFilterSpy.calledOnce).to.be.true
+  })
+
+  it('triggers onFilter() on every submit the form', () => {
+    const onFilterSpy = sinon.spy()
+    const wrapper = mount(<SearchFilters onFilter={onFilterSpy} />)
+
+    wrapper.find('form').simulate('submit')
+    expect(onFilterSpy.calledOnce).to.be.true
+  })
+
+  it('renders a link to the home page when onFilter is not provided', () => {
+    const wrapper = shallow(<SearchFilters />)
+
+    expect(wrapper.find('.brand-logo').length).to.be.eql(1)
+    expect(wrapper.find('form').length).to.be.eql(0)
+  })
+
+  it('renders the filter value when provided by props', () => {
+    const wrapper = shallow(<SearchFilters filter='some value' onFilter={() => {}} />)
+
+    expect(wrapper.find('input').node.props.value).to.be.eql('some value')
   })
 })
