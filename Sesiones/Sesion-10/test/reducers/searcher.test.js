df05fa8ec3650689ad2f860c6c779ab14c30279c
danderu <mail@dandel.es>
2016-10-16 12:36:28 +0200

    Test filter teachers

--- Sesiones/Sesion-10/test/reducers/searcher.test.js
+++ Sesiones/Sesion-10/test/reducers/searcher.test.js
@@ -1,10 +1,25 @@
 import { expect } from 'chai'
 import searcherReducer from '../../src/reducers/searcher'
+import { filterTeachers } from '../../src/actions/searcher'
 
 describe('#searcher reducer', () => {
   it('returns the default state when there is no action to hanle', () => {
     const teachers = []
     const action = {}
     expect(searcherReducer(teachers, action)).to.be.eql(teachers)
+  })
+
+  it('returns the filtered state only with the teachers who matches the filter', () => {
+    const teachers = [{
+      name: 'daniel'
+    }, {
+      name: 'miguel'
+    }]
+
+    const action = filterTeachers({ text: 'dani' })
+
+    const newState = searcherReducer(teachers, action)
+
+    expect(newState.teachers.reduce(t => t)).to.have.property('name', 'Daniel de la Cruz')
   })
 })
