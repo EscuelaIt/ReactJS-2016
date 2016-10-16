f86a939d358dbc024789fa9dfad11442228fa20e
danderu <mail@dandel.es>
2016-10-16 07:11:06 +0200

    first dummy test

---
+++ Sesiones/Sesion-10/test/components/card.test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import Card from '../../src/components/card'
+
+describe('#Card component', () => {
+  it('is rendered properly', () => {
+    expect(Card).to.exist
+  })
+})
