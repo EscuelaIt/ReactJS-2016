7eb9deb5a1b487b4c0fe0d773703123197c8bfe1
danderu <mail@dandel.es>
2016-10-16 12:08:10 +0200

    Use mount and jsdom to simulate events

--- Sesiones/Sesion-10/test/components/searchFilters.test.js
+++ Sesiones/Sesion-10/test/components/searchFilters.test.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { expect } from 'chai'
 import sinon from 'sinon'
 
 import SearchFilters from '../../src/components/searchFilters'
 
 describe('<SearchFilters />', () => {
-  xit('triggers onFilter() on every key press', () => {
-    const callback = () => {}
-    const onFilterSpy = sinon.spy(callback)
-    const wrapper = shallow(<SearchFilters onFilter={callback} />)
+  it('triggers onFilter() on every input change', () => {
+    const onFilterSpy = sinon.spy()
+    const wrapper = mount(<SearchFilters onFilter={onFilterSpy} />)
 
     wrapper.find('input').simulate('change')
-    expect(onFilterSpy).to.have.property('callCount', 1)
+    expect(onFilterSpy.calledOnce).to.be.true
   })
 })
