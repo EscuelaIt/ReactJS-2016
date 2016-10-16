5181e2782f4fe0f0032206a220f86e54c1c49fa1
danderu <mail@dandel.es>
2016-10-16 11:10:09 +0200

    Add enzyme to test cardList

--- Sesiones/Sesion-10/test/components/card.test.js
+++ Sesiones/Sesion-10/test/components/card.test.js
@@ -3,7 +3,7 @@ import { createElement } from 'react'
 import { createRenderer, renderIntoDocument, Simulate } from 'react-addons-test-utils'
 import Card from '../../src/components/card'
 
-describe('#Card component', () => {
+describe('<Card />', () => {
   it('is rendered properly', () => {
     const shallowRenderer = createRenderer()
     const card = createElement(Card)
@@ -12,12 +12,12 @@ describe('#Card component', () => {
     expect(result.type).to.be.eql('div')
   })
 
-  it('link', () => {
-    const shallowRenderer = createRenderer()
-    const card = createElement(Card)
-    shallowRenderer.render(card)
-    const result = shallowRenderer.getRenderOutput()
-    const node = renderIntoDocument(result)
-    Simulate.click(node)
-  })
+  // it('link', () => {
+  //   const shallowRenderer = createRenderer()
+  //   const card = createElement(Card)
+  //   shallowRenderer.render(card)
+  //   const result = shallowRenderer.getRenderOutput()
+  //   const node = renderIntoDocument(result)
+  //   Simulate.click(node)
+  // })
 })
