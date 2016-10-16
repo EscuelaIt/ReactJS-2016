289046293e4c7007b68d2fb2b32ec76bb2a013b0
danderu <mail@dandel.es>
2016-10-16 09:12:18 +0200

    First test with shallowRenderer

--- Sesiones/Sesion-10/test/components/card.test.js
+++ Sesiones/Sesion-10/test/components/card.test.js
@@ -1,8 +1,14 @@
 import { expect } from 'chai'
+import { createElement } from 'react'
+import { createRenderer } from 'react-addons-test-utils'
 import Card from '../../src/components/card'
 
 describe('#Card component', () => {
   it('is rendered properly', () => {
-    expect(Card).to.exist
+    const shallowRenderer = createRenderer()
+    const card = createElement(Card)
+    shallowRenderer.render(card)
+    const result = shallowRenderer.getRenderOutput()
+    expect(result.type).to.be.eql('div')
   })
 })
